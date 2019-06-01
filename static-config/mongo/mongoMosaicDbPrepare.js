83cfe2879ceaabbdc670c37b8f8c24e42c612e4f
planethouki <26595148+planethouki@users.noreply.github.com>
2019-06-01 15:03:32 +0900

    mycow

--- static-config/mongo/mongoMosaicDbPrepare.js
+++ static-config/mongo/mongoMosaicDbPrepare.js
@@ -1,8 +1,7 @@
-(function prepareNamespaceCollections() {
+(function prepareMosaicCollections() {
 	db.createCollection('mosaics');
-	db.mosaics.createIndex({ 'mosaic.mosaicId': 1, 'meta.index': 1 }, { unique: true });
-	db.mosaics.createIndex({ 'meta.active': -1, 'meta.index': 1, 'mosaic.mosaicId': 1 }, { unique: true });
-	db.mosaics.createIndex({ 'meta.active': -1, 'mosaic.definition.owner': 1 });
+	db.mosaics.createIndex({ 'mosaic.mosaicId': 1 }, { unique: true });
+	db.mosaics.createIndex({ 'mosaic.definition.owner': 1 });
 
 	db.mosaics.getIndexes();
 })();
